c5dc8c6c7114ba8bcd0199c5e5c111cdb2fbebb7
kronos93 <desconocido_-_93@hotmail.com>
2017-07-14 03:07:40 -0500

    Versión de frontend
    Se han cargado los recursos y configuraciones para tener el stack del
    frontend funcional

---
+++ edstyles/config/app.php
@@ -0,0 +1,10 @@
+<?php
+
+define('APP_PATH',__DIR__.'/../');
+define('PUBLIC_PATH','http://edstyles.dev/');
+//Composer
+require_once APP_PATH.'vendor/autoload.php';
+//Variables de entorno
+require_once 'env.php';
+//Base de datos
+require_once 'database.php';
---
+++ edstyles/public/index.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once '../config/app.php';
+
+//require_once APP_PATH.'views/pages/index.view.php';
+
+//require_once APP_PATH.'views/articles/index.view.php';
+
+require_once APP_PATH.'views/articles/show.view.php';
